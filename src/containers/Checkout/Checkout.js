5ed9c2eb361c2303b7ce60931403eca1b54542df
keff6 <kev.fallas@gmail.com>
2018-09-05 19:08:27 -0600

    Add ContactData component

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,14 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
   state = {
-    ingrdients: {
+    ingredients: {
       salad: 1,
       meat: 1,
       cheese: 1,
       bacon: 2
     }
+  }
+
+  componentDidMount() {
+    const query = new URLSearchParams(this.props.location.search);
+    const ingredients = {};
+    for(let param of query.entries()){
+      ingredients[param[0]] = +param[1];
+    }
+    this.setState({ ingredients });
   }
 
   checkoutCancelledHandler = () => {
@@ -23,10 +34,11 @@ class Checkout extends Component {
     return (
       <div>
         <CheckoutSummary
-          ingredients={this.state.ingrdients}
+          ingredients={this.state.ingredients}
           checkoutCancelled={this.checkoutCancelledHandler}
           checkoutContinued={this.checkoutContinuedHandler}
         />
+        <Route path={this.props.match.path + '/contact-data'} component={ContactData} />
       </div>
     )
   }
